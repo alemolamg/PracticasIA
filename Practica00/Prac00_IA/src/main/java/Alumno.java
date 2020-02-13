e6e86036296557737aa902209873d5dc7a88807a
molej <molej@DESKTOP-E1MOMVC>
2020-02-13 15:48:29 +0100

    Ver 4.4: Documentación Main terminada, ajustando clase
    fallo de declaración vector en Main::leerArchivo. Debemos ajustar el valor de la longitud del vector

---
+++ Practica00/Prac00_IA/src/main/java/Alumno.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+/**
+ * @author AlemolAMG
+ */
+
+public class Alumno {
+    //  Atributos   //
+    private String nombre;
+    private String dni;
+    String correoE;
+    
+    
+    //  funciones   //
+    
+    /**
+     * Constructor parametrizado clase Alumno
+     * 
+     * @param nombre    (String) nombre alumno
+     * @param dni       (string) documento dni alumno
+     * @param correoE   (string) correo electronico
+     */
+    public Alumno(String nombre, String dni, String correoE) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.correoE = correoE;
+    }
+/**
+ * Constructor por defecto de la clase alumno
+ */
+    public Alumno() {
+        this.nombre =   "Alumno";
+        this.dni =      "1234567895";
+        this.correoE=   "correoE@email.com";
+    }
+    
+   /**
+    * Muestra los datos por pantalla de un Alumno
+    */  
+   void muestraAlumno(){
+       String st1="\nNombre Alumno: " + this.getNombre();
+       String st2="\ndni: "+ getDni();
+       String st3="\ncorreo Electronico: "+this.correoE;
+       
+       System.out.println(st1+st2+st3);
+   } 
+   
+   /**
+    * Actualiza los datos de un alumno escribiendolos
+    */
+    void ActualizarDatosTeclado(){
+       
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduzca nombre: ");
+        this.nombre = teclado.nextLine();
+        System.out.println("Introduzca dni: ");
+        this.dni = teclado.nextLine();
+        System.out.println("Introduzca correo: ");
+        this.correoE = teclado.nextLine();
+        
+   }
+
+    /**
+     * función abstracta no contemplada para un alumno normal
+     */
+    void calcularNotaMedia() {
+        throw new UnsupportedOperationException("calcularNotaMedia::Alumno no implementada"); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Devuelve el nombre del alumno
+     * @return nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve el dni del Alumno
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+        
+}
